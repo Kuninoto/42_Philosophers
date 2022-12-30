9084edea82815496bb01353e3bbbecfd95dea520
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-12-30 00:48:50 +0000

    General updates. Segfaulting on pthread_create() or on the returnvalue of thread 1, somehow...

--- lvl_3_philosophers/philo/main.c
+++ lvl_3_philosophers/philo/main.c
@@ -6,60 +6,55 @@
 /*   By: nnuno-ca <nnuno-ca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 12:40:22 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/12/29 18:47:51 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/12/30 00:30:08 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/philosophers.h"
 
+#define debug printf("debug\n")
+
 // t_id stands for thread identifier (id).
 
-int counter;
+int counter = 0;
 
 void	*routine(void *mutex)
 {
-//	pthread_mutex_lock(&mutex);
+//	pthread_mutex_lock(mutex); // lock = 1
 
 	(void)mutex;
-	
-//	unsigned long i = 0;
     counter++;
-//  printf("\n Job %d has started\n", counter);
+ 	printf("\n Thread %d has started\n", counter);
   
-   /*  for (i = 0; i < (0xFFFFFFFF); i++)
-        ; */
-    printf("\n Job %d has finished\n", counter);
-//	pthread_mutex_unlock(&mutex);
-	return (NULL);
+    for (size_t i = 0; i < (0xFFFFFFFF); i++)
+        ;
+
+    printf("\n Thread %d has finished\n", counter);
+//	pthread_mutex_unlock(mutex); // lock = 0
+	return ("teste");
 }
 
-void	create_threads(t_args *args, pthread_t *t_ids)
+void	create_n_join_threads(t_args *args, pthread_t *t_ids)
 {
 	int i;
 
 	i = 0;
 	while (i < args->nbr_of_philo)
 	{
-		if (pthread_create(&t_ids[i++], NULL, routine, NULL) != 0)
+	debug;
+		if (pthread_create(&t_ids[i], NULL, routine(NULL), NULL) != 0)
 		{
 			free(t_ids);
 			on_error("Failed to create a thread");
 		}
-	}
-}
-
-void	join_threads(t_args *args, pthread_t *t_ids)
-{
-	int i;
-
-	i = 0;
-	while (i < args->nbr_of_philo)
-	{
-		if (pthread_join(t_ids[i++], NULL) != 0)
+		if (pthread_join(t_ids[i], NULL) != 0)
 		{
 			free(t_ids);
 			on_error("Failed to join a thread");
 		}
+	//for (int i = 0; i < args.nbr_of_philo; i++)
+		printf("t_ids[%d] = %lu\n", i, t_ids[i]);
+		i++;
 	}
 }
 
@@ -70,19 +65,19 @@ int main(int argc, char **argv)
 	args = init_args();
 	fill_args(&args, argv);
 	
-/* 	pthread_mutex_t shared_fork;
-	if (pthread_mutex_init(&shared_fork, NULL) != 0)
+/* 	pthread_mutex_t mutex;
+	if (pthread_mutex_init(&mutex, NULL) != 0)
         on_error("Mutex init has failed"); */
 
 	pthread_t *t_ids;
 	t_ids = malloc(args.nbr_of_philo * sizeof(pthread_t));
-	create_threads(&args, t_ids);
+	if (!t_ids)
+		on_error("Failed to allocate memory for t_ids");
+	create_n_join_threads(&args, t_ids);
 
-	// equivalent wait() for threads
-	join_threads(&args, t_ids);
 
+//	pthread_mutex_destroy(&mutex);
 	monitoring(&args, DEAD);
-
 	return (EXIT_SUCCESS);	
 }
 
