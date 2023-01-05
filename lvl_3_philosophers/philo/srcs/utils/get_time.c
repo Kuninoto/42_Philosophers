ad593a37a9ec7911c0798f6f8d631ae56af0aced
Nuno Miguel Nuno Carvalho <nnuno-ca@c1r15s12.42porto.com>
2023-01-05 22:01:51 +0000

    Some updates. Missing death notification and somewhy some philosophers dont even act

--- lvl_3_philosophers/philo/srcs/utils/get_time.c
+++ lvl_3_philosophers/philo/srcs/utils/get_time.c
@@ -6,7 +6,7 @@
 /*   By: nnuno-ca <nnuno-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 17:31:08 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2023/01/05 18:35:27 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2023/01/05 21:04:53 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ suseconds_t	get_time(void)
 	struct timeval	now;
 
 	gettimeofday(&now, NULL);
-	return (now.tv_usec);
+	return (now.tv_sec * (unsigned int)1000) + (now.tv_usec / 1000);
 }
--- lvl_3_philosophers/philo/srcs/utils/long_atoi.c
+++ lvl_3_philosophers/philo/srcs/utils/long_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   long_atoi.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nnuno-ca <nnuno-ca@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: nnuno-ca <nnuno-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 15:34:11 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/12/29 15:54:55 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2023/01/05 21:11:57 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,16 @@ int	long_atoi(char *str)
 	{
 		if (str[i] == '-')
 			on_error("Arguments cannot be negative");
-		i++;
+		i += 1;
 	}
 	while (str[i] >= '0' && str[i] <= '9')
-		res = (res * 10) + (str[i++] - 48);
+	{
+		res = (res * 10) + (str[i] - 48);
+		i += 1;
+	}
 	if (res > INT_MAX || res <= INT_MIN)
 		on_error("One of the arguments overflows integer type");
+	else if (res == 0)
+		on_error("Arguments must be bigger than 0");
 	return ((int)res);
 }
